412b6ebaa18aba8b6d5255d1df721a421a7cf3f3
SAKETH <saketh2608@gmail.com>
2026-07-21 22:27:54 +0530

    Time: 2 ms (6.73%) | Memory: 43.8 MB (11.34%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -2,23 +2,27 @@ class Solution {
     public boolean check(int[] nums) {
         int n=nums.length;
         for(int i=0;i<nums.length;i++){
-            rotate(nums,i);
-            if(isSorted(nums))return true;
-            rotate(nums,n-i);
+            // rotate(nums,i);
+            int []r=new int[n];
+            for(int j=0;j<n;j++){
+                r[j]=nums[(i+j)%n];
+            }
+            if(isSorted(r))return true;
+            // rotate(nums,n-i);
         }
         return false;
     }
-    void swap(int []a,int i,int j){
-        int t=a[i];
-        a[i]=a[j];
-        a[j]=t;
-    }
-    void rotate(int []a,int k){
-        int n=a.length;
-            for(int j=0;j<k/2;j++)swap(a,j,k-j-1);
-            for(int j=k;j<k+(n-k)/2;j++)swap(a,j,n-j+k-1);
-            for(int j=0;j<n/2;j++)swap(a,j,n-j-1);
-    }
+    // void swap(int []a,int i,int j){
+    //     int t=a[i];
+    //     a[i]=a[j];
+    //     a[j]=t;
+    // }
+    // void rotate(int []a,int k){
+    //     int n=a.length;
+    //         for(int j=0;j<k/2;j++)swap(a,j,k-j-1);
+    //         for(int j=k;j<k+(n-k)/2;j++)swap(a,j,n-j+k-1);
+    //         for(int j=0;j<n/2;j++)swap(a,j,n-j-1);
+    // }
     boolean isSorted(int []a){
         for(int i=1;i<a.length;i++){
             if(a[i]<a[i-1])return false;
