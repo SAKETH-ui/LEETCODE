f6db52e6ae378b1925853decec01f46b2359b816
SAKETH <saketh2608@gmail.com>
2026-08-01 22:51:19 +0530

    Time: 10 ms (74.82%) | Memory: 138.2 MB (65.47%) - LeetSync

--- 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.java
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.java
@@ -1,22 +1,14 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int i=0,ans=0;
         int n=nums.length;
-        while(true){
             Set<Integer> s=new HashSet<>();
-            boolean f=false;
-            for(int j=n-1;j>=i;j--){
+            for(int j=n-1;j>=0;j--){
                 if(s.contains(nums[j])){
-                    f=true;
                     return (j/3)+1;
                     
                 }
                 s.add(nums[j]);
             }
-            if(f==false)break;
-            i+=3;
-            ans++;
-        }
-        return ans;
+        return 0;
     }
 }
