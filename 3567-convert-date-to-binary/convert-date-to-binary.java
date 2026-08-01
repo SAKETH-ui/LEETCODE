31370913abc1c53ec2f69aaa0ea28dcffd8843a6
SAKETH <saketh2608@gmail.com>
2026-08-01 19:18:45 +0530

    Time: 3 ms (91.58%) | Memory: 44 MB (36.75%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.java
+++ 3567-convert-date-to-binary/convert-date-to-binary.java
@@ -5,23 +5,46 @@ class Solution {
         y=Integer.parseInt(s[0]);
         m=Integer.parseInt(s[1]);
         d=Integer.parseInt(s[2]);
-        String yy="";
+        // String yy="";
+        // while(y>0){
+        //     yy=(y%2)+yy;
+        //     y/=2;
+        // }
+        // String mm="";
+        // while(m>0){
+        //     mm=(m%2)+mm;
+        //     m/=2;
+        // }
+        // String dd="";
+        // while(d>0){
+        //     dd=(d%2)+dd;
+        //     d/=2;
+        // }
+        // String r="";
+        // r+=yy+"-"+mm+"-"+dd;
+        // return r;
+        StringBuilder yy=new StringBuilder();
+        StringBuilder mm=new StringBuilder();
+        StringBuilder dd=new StringBuilder();
         while(y>0){
-            yy=(y%2)+yy;
+            yy.append(y%2);
             y/=2;
         }
-        String mm="";
+        yy=yy.reverse();
+        yy.append("-");
         while(m>0){
-            mm=(m%2)+mm;
+            mm.append(m%2);
             m/=2;
         }
-        String dd="";
+        mm=mm.reverse();
+        yy.append(mm);
+        yy.append("-");
         while(d>0){
-            dd=(d%2)+dd;
+            dd.append(d%2);
             d/=2;
         }
-        String r="";
-        r+=yy+"-"+mm+"-"+dd;
-        return r;
+        dd=dd.reverse();
+        yy.append(dd);
+        return yy.toString();
     }
 }
