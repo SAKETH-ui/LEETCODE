be1e6613ab494156b034030d61a18ec183c056de
SAKETH <saketh2608@gmail.com>
2026-08-04 19:52:09 +0530

    Time: 1 ms (75.49%) | Memory: 43.6 MB (26.88%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,8 +1,8 @@
 class Solution {
     public int numOfStrings(String[] nums, String w) {
         int ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(w.contains(nums[i]))ans++;
+        for(String s:nums){
+            if(w.contains(s))ans++;
         }
         return ans;
     }
