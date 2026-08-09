c41a78b56f3cfa92831c9d943cf4bc76c0cdf214
SAKETH <saketh2608@gmail.com>
2026-08-10 00:37:17 +0530

    Time: 2 ms (99.96%) | Memory: 52.9 MB (22.22%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -3,23 +3,32 @@ import java.util.*;
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         // Code here
-        int n=nums.length;
-        int l=n/3;
-        
-        HashMap<Integer,Integer> a=new HashMap<>();
-        
-        for(int i=0;i<n;i++){
-            a.put(nums[i],a.getOrDefault(nums[i],0)+1);
-        }
-        
         List<Integer> list=new ArrayList<>();
-        
-        for(Map.Entry<Integer,Integer> entry : a.entrySet()){
-            if(entry.getValue()>l)list.add(entry.getKey());
+        int n=nums.length,cnt1=0,cnt2=0,ele1=0,ele2=0;
+        int l=n/3;
+        for(int i=0;i<n;i++){
+            if(ele1==nums[i])cnt1++;
+            else if(ele2==nums[i])cnt2++;
+            else if(cnt1==0&&ele2!=nums[i]){
+                ele1=nums[i];
+                cnt1=1;
+            }
+            else if(cnt2==0&& ele1!=nums[i]){
+                ele2=nums[i];
+                cnt2=1;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
         }
-        
-        Collections.sort(list);
-        
+        int c1=0,c2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele1)c1++;
+            else if(nums[i]==ele2)c2++;
+        }
+        if(c1>l)list.add(ele1);
+        if(c2>l)list.add(ele2);
         return list;
     }
 }
