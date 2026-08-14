aab1864fcf665d98a5420055ab403c2b2bfee3eb
SAKETH <saketh2608@gmail.com>
2026-08-14 10:26:38 +0530

    Time: 273 ms (5.96%) | Memory: 47.3 MB (54.11%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int m=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int p=1;
+            for(int j=i;j<nums.length;j++){
+                p*=nums[j];
+            m=Math.max(m,p);
+            }
+        }
+        return m;
+    }
+}
