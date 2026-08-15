1ad120d2033bfed730b6e6a1c589ea48922f870e
SAKETH <saketh2608@gmail.com>
2026-08-15 23:44:15 +0530

    Time: 1 ms (61.13%) | Memory: 43.4 MB (16.97%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,18 +1,15 @@
 import java.util.*;
 class Solution {
     public String longestCommonPrefix(String[] str) {
-        String s="";
         if(str.length==0)return "";
             for(int i=0;i<str[0].length();i++){
-                for(int j=0;j<str.length;j++){
-                    
+                for(int j=1;j<str.length;j++){
                     if(i==str[j].length()||str[0].charAt(i)!=(str[j].charAt(i))){
-                        return s;
+                        return str[0].substring(0,i);
                     }
                 }
-                s+=str[0].charAt(i);
             }
             
-            return s;
+            return str[0];
     }
 }
