db791a42e40885f9d55674b853bcdbef26a9961a
SAKETH <saketh2608@gmail.com>
2026-08-16 22:39:24 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (9.08%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -5,10 +5,16 @@ class Solution {
         int l=0,r=n-1;
       while(l<r){
         int m=(l+r)/2;
-        
-        if(m%2==1)m--;
-        if(a[m]==a[m+1])l=m+2;
-        else r=m;
+        if(m>0&&a[m]==a[m-1]){
+            if(m%2==0)r=m-1;
+            else l=m+1;
+        }
+        else if(m<n-1&&a[m]==a[m+1]){
+            if(m%2==0)l=m+1;
+            else r=m-1;
+        }
+        else 
+        return a[m];
       }
       return a[l];
     }
