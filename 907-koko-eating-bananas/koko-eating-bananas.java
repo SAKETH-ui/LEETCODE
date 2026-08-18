8f79d1bf17232e6b879f41cda88bd6c9f1548fa4
SAKETH <saketh2608@gmail.com>
2026-08-18 22:17:23 +0530

    Time: 6 ms (99.67%) | Memory: 48.1 MB (32.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
        int max = piles[0];
-       Solution sc=new Solution();
         for (int pile : piles) {
             if (pile > max) 
             max = pile;
@@ -9,7 +8,7 @@ class Solution {
         int low=1,high=max,ans=0;
         while(low<=high){
             int m=low+(high-low)/2;
-            long total=sc.TH(piles,m);
+            long total=TH(piles,m);
             if(total>h){
                 low=m+1;
             }
@@ -20,11 +19,11 @@ class Solution {
         }
         return ans;
 }
-public long TH(int []a,int r){
+static long TH(int []a,int r){
         long ans=0;
         for(int i=0;i<a.length;i++){
-            double h=(double)a[i]/r;
-            ans+=(int)Math.ceil(h);
+            ans+=(a[i]+r-1)/r;
+            
         }
         return ans;
     }
